60601e455a128d6ba8a0f5a05e2dcd73065baa61
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2022-05-08 13:29:07 +0200

    Docker update

--- startup/config/config.go
+++ startup/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	Port           string
 	ProfileHost    string
@@ -13,15 +18,33 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	return &Config{
-		Port:           "8000",      //os.Getenv("GATEWAY_PORT"),
-		ProfileHost:    "localhost", //os.Getenv("PROFILE_SERVICE_HOST"),
-		ProfilePort:    "8001",      //os.Getenv("PROFILE_SERVICE_PORT"),
-		PostHost:       "localhost", //os.Getenv("POST_SERVICE_HOST"),
-		PostPort:       "8002",      //os.Getenv("POST_SERVICE_PORT"),
-		AuthHost:       "localhost", //os.Getenv("AUTH_SERVICE_HOST"),
-		AuthPort:       "8003",      //os.Getenv("AUTH__SERVICE_PORT"),
-		ConnectionHost: "localhost",
-		ConnectionPort: "8004",
+	if _, err := os.Stat("/.dockerenv"); err == nil {
+		fmt.Println("docker")
+
+		return &Config{
+			Port:           os.Getenv("GATEWAY_PORT"),
+			ProfileHost:    os.Getenv("PROFILE_SERVICE_HOST"),
+			ProfilePort:    os.Getenv("PROFILE_SERVICE_PORT"),
+			PostHost:       os.Getenv("POST_SERVICE_HOST"),
+			PostPort:       os.Getenv("POST_SERVICE_PORT"),
+			AuthHost:       os.Getenv("AUTHENTICATION_SERVICE_HOST"),
+			AuthPort:       os.Getenv("AUTHENTICATION_SERVICE_PORT"),
+			ConnectionHost: os.Getenv("CONNECTION_SERVICE_HOST"),
+			ConnectionPort: os.Getenv("CONNECTION_SERVICE_PORT"),
+		}
+	} else {
+		fmt.Println("local")
+
+		return &Config{
+			Port:           "8000",
+			ProfileHost:    "localhost",
+			ProfilePort:    "8001",
+			PostHost:       "localhost",
+			PostPort:       "8002",
+			AuthHost:       "localhost",
+			AuthPort:       "8003",
+			ConnectionHost: "localhost",
+			ConnectionPort: "8004",
+		}
 	}
 }
